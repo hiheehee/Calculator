88e602234886d9af3f37edbd70957cbc2b4ba801
hiheehee <gkek41@naver.com>
2021-01-02 18:05:17 +0900

    add exception handling

--- calculator/src/calculator/calculator.java
+++ calculator/src/calculator/calculator.java
@@ -44,134 +44,134 @@ public class calculator {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 600);
+		frame.setBounds(100, 100, 435, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel answer = new JLabel("0");
 		answer.setHorizontalAlignment(SwingConstants.RIGHT);
 		answer.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		answer.setBounds(12, 10, 395, 69);
+		answer.setBounds(10, 10, 395, 70);
 		frame.getContentPane().add(answer);
 		
 		JButton btnLeft = new JButton("(");
 		btnLeft.addActionListener(new ButtonActListener(answer, "("));
-		btnLeft.setBounds(10, 89, 90, 75);
+		btnLeft.setBounds(10, 90, 90, 75);
 		btnLeft.setFont(new Font("±¼¸²", Font.BOLD, 30));
 		frame.getContentPane().add(btnLeft);
 		
 		JButton btn7 = new JButton("7");
 		btn7.addActionListener(new ButtonActListener(answer, "7"));
-		btn7.setBounds(10, 174, 90, 75);
+		btn7.setBounds(10, 175, 90, 75);
 		btn7.setFont(new Font("±¼¸²", Font.BOLD, 30));
 		frame.getContentPane().add(btn7);
 		
 		JButton btn4 = new JButton("4");
 		btn4.addActionListener(new ButtonActListener(answer, "4"));
 		btn4.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btn4.setBounds(10, 259, 90, 75);
+		btn4.setBounds(10, 260, 90, 75);
 		frame.getContentPane().add(btn4);
 		
 		JButton btn1 = new JButton("1");
 		btn1.addActionListener(new ButtonActListener(answer, "1"));
 		btn1.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btn1.setBounds(10, 344, 90, 75);
+		btn1.setBounds(10, 345, 90, 75);
 		frame.getContentPane().add(btn1);
 		
 		JButton btn0 = new JButton("0");
 		btn0.addActionListener(new ButtonActListener(answer, "0"));
 		btn0.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btn0.setBounds(10, 429, 90, 75);
+		btn0.setBounds(10, 430, 90, 75);
 		frame.getContentPane().add(btn0);
 		
 		JButton btnRight = new JButton(")");
 		btnRight.addActionListener(new ButtonActListener(answer, ")"));
-		btnRight.setBounds(112, 89, 90, 75);
+		btnRight.setBounds(112, 90, 90, 75);
 		btnRight.setFont(new Font("±¼¸²", Font.BOLD, 30));
 		frame.getContentPane().add(btnRight);
 		
 		JButton btn8 = new JButton("8");
 		btn8.addActionListener(new ButtonActListener(answer, "8"));
 		btn8.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btn8.setBounds(112, 174, 90, 75);
+		btn8.setBounds(112, 175, 90, 75);
 		frame.getContentPane().add(btn8);
 		
 		JButton btn5 = new JButton("5");
 		btn5.addActionListener(new ButtonActListener(answer, "5"));
 		btn5.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btn5.setBounds(112, 259, 90, 75);
+		btn5.setBounds(112, 260, 90, 75);
 		frame.getContentPane().add(btn5);
 		
 		JButton btn2 = new JButton("2");
 		btn2.addActionListener(new ButtonActListener(answer, "2"));
 		btn2.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btn2.setBounds(112, 344, 90, 75);
+		btn2.setBounds(112, 345, 90, 75);
 		frame.getContentPane().add(btn2);
 		
 		JButton btnDot = new JButton(".");
 		btnDot.addActionListener(new ButtonActListener(answer, "."));
 		btnDot.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btnDot.setBounds(112, 429, 90, 75);
+		btnDot.setBounds(112, 430, 90, 75);
 		frame.getContentPane().add(btnDot);
 		
 		JButton btnPer = new JButton("%");
 		btnPer.addActionListener(new ButtonActListener(answer, "%"));
-		btnPer.setBounds(214, 89, 90, 75);
+		btnPer.setBounds(214, 90, 90, 75);
 		btnPer.setFont(new Font("±¼¸²", Font.BOLD, 30));
 		frame.getContentPane().add(btnPer);
 		
 		JButton btn9 = new JButton("9");
 		btn9.addActionListener(new ButtonActListener(answer, "9"));
 		btn9.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btn9.setBounds(214, 174, 90, 75);
+		btn9.setBounds(214, 175, 90, 75);
 		frame.getContentPane().add(btn9);
 		
 		JButton btn6 = new JButton("6");
 		btn6.addActionListener(new ButtonActListener(answer, "6"));
 		btn6.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btn6.setBounds(214, 259, 90, 75);
+		btn6.setBounds(214, 260, 90, 75);
 		frame.getContentPane().add(btn6);
 		
 		JButton btn3 = new JButton("3");
 		btn3.addActionListener(new ButtonActListener(answer, "3"));
 		btn3.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btn3.setBounds(214, 344, 90, 75);
+		btn3.setBounds(214, 345, 90, 75);
 		frame.getContentPane().add(btn3);
 		
 		JButton btnEqual = new JButton("=");
 		btnEqual.addActionListener(new ButtonActListener(answer, "="));
 		btnEqual.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btnEqual.setBounds(214, 429, 90, 75);
+		btnEqual.setBounds(214, 430, 90, 75);
 		frame.getContentPane().add(btnEqual);
 		
 		JButton btnAllClear = new JButton("AC");
 		btnAllClear.addActionListener(new ButtonActListener(answer, "AC"));
-		btnAllClear.setBounds(317, 89, 90, 75);
+		btnAllClear.setBounds(317, 90, 90, 75);
 		btnAllClear.setFont(new Font("±¼¸²", Font.BOLD, 30));
 		frame.getContentPane().add(btnAllClear);
 		
 		JButton btnPlus = new JButton("+");
 		btnPlus.addActionListener(new ButtonActListener(answer, "+"));
 		btnPlus.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btnPlus.setBounds(316, 174, 90, 75);
+		btnPlus.setBounds(316, 175, 90, 75);
 		frame.getContentPane().add(btnPlus);
 		
 		JButton btnSub = new JButton("-");
 		btnSub.addActionListener(new ButtonActListener(answer, "-"));
 		btnSub.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btnSub.setBounds(316, 259, 90, 75);
+		btnSub.setBounds(316, 260, 90, 75);
 		frame.getContentPane().add(btnSub);
 		
 		JButton btnMul = new JButton("X");
 		btnMul.addActionListener(new ButtonActListener(answer, "X"));
 		btnMul.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btnMul.setBounds(317, 344, 90, 75);
+		btnMul.setBounds(317, 345, 90, 75);
 		frame.getContentPane().add(btnMul);
 		
 		JButton btnDiv = new JButton("/");
 		btnDiv.addActionListener(new ButtonActListener(answer, "/"));
 		btnDiv.setFont(new Font("±¼¸²", Font.BOLD, 30));
-		btnDiv.setBounds(316, 429, 90, 75);
+		btnDiv.setBounds(316, 430, 90, 75);
 		frame.getContentPane().add(btnDiv);
 		
 	}
@@ -218,6 +218,13 @@ public class calculator {
 							this.label.setText(this.label.getText() + this.text);
 						}
 					}
+				}
+			}else if(cur.equals(")")){
+				char preString = this.label.getText().charAt(this.label.getText().length()-1);
+				if(preString == '(' || preString == '+' || preString == '-' || preString == 'X' || preString == '/') { 
+					this.label.setText(this.label.getText());
+				}else {
+					this.label.setText(this.label.getText() + this.text);
 				}
 			}else {
 				if(this.label.getText().equals("0")) { 
@@ -325,8 +332,6 @@ public class calculator {
 			postfix.add(opStack.pop().toString());
 		}
 		
-		System.out.println(postfix);
-		
 		// ÈÄÀ§ ¿¬»êÀÚ¸¦ ÀÌ¿ëÇØ ÃÖÁ¾ °á°ú ±¸ÇÏ±â
 		for(int i = 0; i < postfix.size(); i++) {
 			calStack.push(postfix.get(i));
@@ -358,8 +363,7 @@ public class calculator {
 		
 		Double result = Double.parseDouble(calStack.pop());
 		String calResult[] = Double.toString(result).split("\\.");
-		System.out.println(result + " "+ calResult[0]+ " "+ Double.parseDouble(calResult[1])*10000);
-		
+
 		if(Double.parseDouble(calResult[1]) == 0) {  
 			if(3 <= calResult[1].length() && calResult[1].substring(0, 2).equals("0E")) {
 				return Double.toString(result);
